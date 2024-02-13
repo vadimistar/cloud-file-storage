8ddfea0fa35b0f2813be04ee555878edbf0ebfe6
vadimistar <vadimstar@gmail.com>
2024-02-13 10:35:25 +0300

    Init page

---
+++ src/main/java/com/vadimistar/cloudfilestorage/CloudfilestorageApplication.java
@@ -0,0 +1,13 @@
+package com.vadimistar.cloudfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudfilestorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudfilestorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vadimistar/cloudfilestorage/CloudfilestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadimistar.cloudfilestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudfilestorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
