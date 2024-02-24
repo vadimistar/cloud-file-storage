b27ea61977bc705dc51ebd6894802039081e57f8
vadimistar <vadimstar@gmail.com>
2024-02-24 20:08:24 +0300

    Add filename validation

--- src/main/java/com/vadimistar/cloudfilestorage/dto/RenameRequestDto.java
+++ src/main/java/com/vadimistar/cloudfilestorage/dto/RenameRequestDto.java
@@ -1,6 +1,7 @@
 package com.vadimistar.cloudfilestorage.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -10,5 +11,6 @@ public class RenameRequestDto {
     private String path;
 
     @NotBlank(message = "Name cannot be blank")
+    @Pattern(regexp = "^([0-9]|[A-Z]|[a-z]|[!\\-_ .*'\\(\\)])+$", message = "Name contains invalid characters")
     private String name;
 }
