e98c26f1cadca048990ae481b02b81de66e2e136
vadimistar <vadimistar@gmail.com>
2024-03-01 16:58:23 +0300

    Fix bug: null files are displayed as folders, so forbid null files upload

---
+++ src/main/java/com/vadimistar/cloudfilestorage/exceptions/UploadFileException.java
@@ -0,0 +1,8 @@
+package com.vadimistar.cloudfilestorage.exceptions;
+
+public class UploadFileException extends FileServiceException {
+
+    public UploadFileException(String message) {
+        super(message);
+    }
+}
