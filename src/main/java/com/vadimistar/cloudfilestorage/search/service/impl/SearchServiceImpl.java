0034966e33a4456a462e33acd01373437e1366d3
vadimistar <vadimstar@gmail.com>
2024-03-03 21:25:05 +0300

    Move file dto to found file dto mapping from controller to service

--- src/main/java/com/vadimistar/cloudfilestorage/search/service/impl/SearchServiceImpl.java
+++ src/main/java/com/vadimistar/cloudfilestorage/search/service/impl/SearchServiceImpl.java
@@ -2,6 +2,8 @@ package com.vadimistar.cloudfilestorage.search.service.impl;
 
 import com.vadimistar.cloudfilestorage.common.dto.FileDto;
 import com.vadimistar.cloudfilestorage.folder.service.FolderService;
+import com.vadimistar.cloudfilestorage.search.dto.FoundFileDto;
+import com.vadimistar.cloudfilestorage.search.mapper.FoundFileDtoMapper;
 import com.vadimistar.cloudfilestorage.search.service.SearchService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,8 +17,9 @@ public class SearchServiceImpl implements SearchService {
     private final FolderService folderService;
 
     @Override
-    public Stream<FileDto> searchFiles(long userId, String query) {
+    public Stream<FoundFileDto> searchFiles(long userId, String query) {
         return folderService.getAllContent(userId)
-                .filter(file -> file.getName().contains(query));
+                .filter(file -> file.getName().contains(query))
+                .map(FoundFileDtoMapper::makeFoundFileDto);
     }
 }
