c9a673a558baa2cea88227f82728508d1faa9e75
vadimistar <vadimistar@gmail.com>
2024-03-17 12:51:22 +0300

    Set common request mapping for login controller

--- src/main/java/com/vadimistar/cloudfilestorage/security/controller/LoginController.java
+++ src/main/java/com/vadimistar/cloudfilestorage/security/controller/LoginController.java
@@ -1,14 +1,14 @@
 package com.vadimistar.cloudfilestorage.security.controller;
 
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@AllArgsConstructor
+@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping("/login")
+    @GetMapping
     public String login() {
         return "login";
     }
