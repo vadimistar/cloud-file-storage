ed5fd33e17e72d6f3f3bbc85f4317f32d71252a7
vadimistar <vadimistar@gmail.com>
2024-03-22 20:09:33 +0300

    Remove unused constructor

--- src/main/java/com/vadimistar/cloudfilestorage/security/exception/UserNotLoggedInException.java
+++ src/main/java/com/vadimistar/cloudfilestorage/security/exception/UserNotLoggedInException.java
@@ -5,8 +5,4 @@ public class UserNotLoggedInException extends RuntimeException {
     public UserNotLoggedInException(String message) {
         super(message);
     }
-
-    public UserNotLoggedInException() {
-        this("You are not logged in, please login again");
-    }
 }
