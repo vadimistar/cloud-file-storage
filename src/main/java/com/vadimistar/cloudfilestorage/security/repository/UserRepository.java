2f9c0635e31b6be914620dffbe66676e2b23f097
vadimistar <vadimistar@gmail.com>
2024-03-25 10:33:13 +0300

    Fix bug: repository does not extend JpaRepository although jpa is used in project

--- src/main/java/com/vadimistar/cloudfilestorage/security/repository/UserRepository.java
+++ src/main/java/com/vadimistar/cloudfilestorage/security/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package com.vadimistar.cloudfilestorage.security.repository;
 
 import com.vadimistar.cloudfilestorage.security.entity.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> getUserByUsername(String username);
     Optional<User> getUserByUsernameOrEmail(String username, String email);
